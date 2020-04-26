04095b3a79f7a65cacf2ca137048ff33636d5c30
Bruermar <56125810+Bruermar@users.noreply.github.com>
2020-04-26 11:26:38 +0200

    Update server.ts

--- server/server.ts
+++ server/server.ts
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
-const port = process.env.PORT || 8080;
+const port = process.env.PORT;
 
 process.on("unhandledRejection", (reason, p) => {
     console.log("Unhandled Rejection at:", p, "reason:", reason);
@@ -80,10 +80,11 @@ TestData.init();
 // set the home page route
 app.get("/", function (req, res) {
 
-    // ejs render automatically looks in the views folder
-    res.render("../index.html");
+  // ejs render automatically looks in the views folder¨
+  //   express.static("../index.html");
+  res.sendFile("../index.html");
 });
 
 app.listen(port, function () {
-    console.log("Path example server running on http://localhost:" + port);
+    console.log("Path example server running on Port:" + port);
 });
